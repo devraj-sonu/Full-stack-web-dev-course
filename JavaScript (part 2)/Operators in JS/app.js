0224790de89b42f8a1d0075eeb6f2bc0a157725d
devraj-sonu <devraj6477@gmail.com>
2026-06-18 10:33:51 +0530

    add operators in javascript folder in git repo

---
+++ JavaScript (part 2)/Operators in JS/app.js
@@ -0,0 +1,12 @@
+//Arithmetic Operators
+
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a**b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(++a);
