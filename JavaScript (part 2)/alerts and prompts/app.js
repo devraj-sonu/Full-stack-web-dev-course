57af063f16aac0141d21ddd170e3d3aa67f4b7bc
devraj-sonu <devraj6477@gmail.com>
2026-06-18 18:22:38 +0530

    add javascript part 2 file

---
+++ JavaScript (part 2)/alerts and prompts/app.js
@@ -0,0 +1,3 @@
+let firstName = prompt("enter first name :");
+let lastname = prompt("enter last name :");
+console.log("welcome",firstName, lastname, "!");
---
+++ JavaScript (part 2)/truthyfalsy.js
@@ -0,0 +1,11 @@
+if(1) {
+    console.log("it has true value");
+} else {
+    console.log("it has false value");
+} 
+
+if(undefined) {
+    console.log("it has true value");
+} else {
+    console.log("it has false value");
+}
