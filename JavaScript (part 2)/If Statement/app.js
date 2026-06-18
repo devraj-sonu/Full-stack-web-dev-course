3ef3dc3ea3a36c99bb082fc8a47511b12b783814
devraj-sonu <devraj6477@gmail.com>
2026-06-18 10:45:21 +0530

    add if conditional statement in git repo

---
+++ JavaScript (part 2)/If Statement/app.js
@@ -0,0 +1,18 @@
+//conditional statement
+
+console.log("before my if statement");
+let age = 23;
+if(age>=18) {
+    console.log("you can vote");
+}
+if(age<18) {
+    console.log("You can not vote right now");
+}
+console.log("after my if statement");
+
+//conditional statement
+
+let firstName = "Devraj";
+if(firstName=="Devraj") {
+    console.log(`welcome ${firstname}`);
+}
