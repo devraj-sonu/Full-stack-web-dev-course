4d6c1490626f1c10bdf8e8c1248a28aa4c14d91e
devraj-sonu <devraj6477@gmail.com>
2026-06-18 11:16:47 +0530

    add practice qs folder in repo

---
+++ JavaScript (part 2)/Practice Qs/app.js
@@ -0,0 +1,13 @@
+let color = "red";
+
+//traffic light system
+
+if(color === "red") {
+    console.log("stop! the light is red")
+}
+if(color === "yellow") {
+    console.log("slow down the light is yellow");
+}
+if(color === "green") {
+    console.log("you can go now");
+}
