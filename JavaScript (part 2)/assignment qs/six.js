81884f109c9c9c714a4a4a4451a0b7a6492d0f29
devraj-sonu <devraj6477@gmail.com>
2026-06-23 12:30:27 +0530

    Add javascript part 2

---
+++ JavaScript (part 2)/assignment qs/six.js
@@ -0,0 +1,8 @@
+let num1 = 32;
+let num2 = 47852;
+
+if((num1%10)==(num2%10)) {
+    console.log("numbers have the same jast digit wgich is :", num1%10 );
+} else {
+    console.log("numbers don't have the same last digit");
+}
---
+++ JavaScript (part 2)/assignment qs/two.js
@@ -0,0 +1,5 @@
+let name = prompt("please enter your name : ");
+let age = prompt("please enter your age : ");
+
+
+alert(`${name} is ${age} years old.`);
