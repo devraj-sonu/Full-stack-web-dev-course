427112e84c74c54ca381b482ff85ef786b5d6496
devraj-sonu <devraj6477@gmail.com>
2026-06-27 20:16:09 +0530

    add get values folder in repo

---
+++ JavaScript(part 5)/get values/app.js
@@ -0,0 +1,13 @@
+let post = {
+    username = "devraj",
+    likes = 150,
+    reposts = 10,
+    tags = [devrajgiri, apnacollege],
+}
+
+post["content"];
+post["likes"];
+post.content;
+
+let prop = reposts;
+post[prop];
