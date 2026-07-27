d4bd7c084936468469d59b48dc6d9edcca5cecee
devraj-sonu <devraj6477@gmail.com>
2026-07-27 19:35:25 +0530

    add random number practice qs file

---
+++ JavaScript(part 5)/practice qs/app.js
@@ -0,0 +1,11 @@
+// generate a random number between 1 to 100
+
+Math.floor(Math.randon()*100)+1;
+
+// to get a number between 1 to 5
+
+Math.floor(Math.random()*5)+1;
+
+//to get a number from or between 20 to 25
+
+Math.floor(Math.random()*25)+20;
