b53576903dd86f47c6d11a39e71cfa3b78597747
devraj-sonu <devraj6477@gmail.com>
2026-08-08 09:21:31 +0530

    Add assignment solution in git repo

---
+++ JavaScript(part 5)/Assignment sol/app2.js
@@ -0,0 +1,7 @@
+const car = {
+    name: "Maruti Suzuki",
+    model: "Maruti Suzuki Dzire",
+    color: "white",
+};
+
+console.log(car.name);
---
+++ JavaScript(part 5)/Assignment sol/app3.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "jane Doe",
+    age: 21,
+    city: "New Jersey",
+};
+
+person.city = "New York";
+person.country = "United States";
+console.log(person);
