13607d95bfc1454853684c410daac07601b7fc87
devraj-sonu <devraj6477@gmail.com>
2026-08-08 09:47:17 +0530

    add function defination in repo

---
+++ JavaScript (part 6)/function defination/app.js
@@ -0,0 +1,31 @@
+function hello() {
+    console.log("hello");
+}
+
+hello();
+
+function printName() {
+    console.log("apna college")
+}
+
+printName();
+
+function oneto5() {
+    for(let i=1; i<=5; i++) {
+        console.log(i);
+    }
+}
+
+oneto5();
+
+
+function isAdult() {
+    let age = 18;
+    if(age>18) {
+        console.log("adult");
+    } else {
+        console.log("not adult");
+    }
+}
+
+isAdult();
