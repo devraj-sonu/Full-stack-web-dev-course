bb70e9a6324eafdada8ff2821f4b888862eb79ee
devraj-sonu <devraj6477@gmail.com>
2026-08-10 19:52:25 +0530

    add function with argument folder in git repo

---
+++ JavaScript (part 6)/function with argument/app.js
@@ -0,0 +1,10 @@
+function printName(name) {
+    console.log(name);
+}
+
+function printInfo(name,age) {
+    console.log(`${name}'s age is ${age}.`);
+}
+
+printName("Devraj");
+printInfo("Devraj","23");
