0bb8efd7a164db141f6c176473ef196960d03f76
devraj-sonu <devraj6477@gmail.com>
2026-08-14 11:34:47 +0530

    add foa folder some changes

--- JavaScript (part 6)/function with argument/app.js
+++ JavaScript (part 6)/function with argument/app.js
@@ -6,5 +6,10 @@ function printInfo(name,age) {
     console.log(`${name}'s age is ${age}.`);
 }
 
+function sum(a,b) {
+    console.log(a+b);
+}
+
 printName("Devraj");
 printInfo("Devraj","23");
+sum(a,b);
