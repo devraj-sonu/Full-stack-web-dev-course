85489469b7181db9237c486a5bea548ac50f76c7
devraj-sonu <devraj6477@gmail.com>
2026-08-15 10:36:33 +0530

    add practice qs 6 in repo

---
+++ JavaScript (part 6)/practice qs 6/app.js
@@ -0,0 +1,13 @@
+// function to return the sum of numbers from 1 to n
+
+
+function getSum(n) {
+    let sum = 0;
+
+    for(let i=1; i<=n; i++) {
+        sum += i;
+    }
+
+    return sum;
+    
+}
