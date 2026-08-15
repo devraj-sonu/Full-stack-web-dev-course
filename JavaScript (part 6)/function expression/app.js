125b3937c2ebb3ca95c9122c0b04a00d70a40119
devraj-sonu <devraj6477@gmail.com>
2026-08-15 21:19:56 +0530

    add function expression folder in git repo

---
+++ JavaScript (part 6)/function expression/app.js
@@ -0,0 +1,15 @@
+let name = "devraj";
+
+let sum = function(a,b) {
+    return a+b;
+}
+
+let hello = function() {
+    console.log("hello");
+}
+
+// to update the value of hello we use another function
+
+hello = function() {
+    console.log("namaste");
+}
